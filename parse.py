ead40d55ccc90768a337c70129352cb0567b8507
Vanessa Sabino <borboleta@gmail.com>
2014-04-27 21:52:30 -0400

    group events in dictionary

--- parse.py
+++ parse.py
@@ -16,31 +16,40 @@ def read(filename):
       if header:
         event_key = tuple(row[14:])
       else:
-        sleep = {}
-        start = datetime.strptime(row[2], '%d. %m. %Y %H:%M')
-        end = datetime.strptime(row[3], '%d. %m. %Y %H:%M')
-        date = start.date()
-        if start.hour < 12:
-          date = date - timedelta(1)
-        sleep['date'] = date.isoformat()
-        sleep['weekday'] = date.isoweekday() # Mon: 1 - Sun: 7
-        sleep['start_hour'] = start.time()
-        sleep['end_hour'] = end.time()
-        sleep['length'] = (end - start).total_seconds() / 3600
-        sleep['cycles'] = int(row[11])
-        sleep['deep'] = float(row[12])
-        data_sleep.append(sleep)
+        data_sleep.append(parse_sleep(row))
         event_value = tuple(row[14:])
         data_events.append(filter_events(event_key, event_value))
-    # import pdb; pdb.set_trace()
+    import pdb; pdb.set_trace()
+
+def parse_sleep(row):
+  sleep = {}
+  start = datetime.strptime(row[2], '%d. %m. %Y %H:%M')
+  end = datetime.strptime(row[3], '%d. %m. %Y %H:%M')
+  date = start.date()
+  if start.hour < 12:
+    date = date - timedelta(1)
+  sleep['date'] = date.isoformat()
+  sleep['weekday'] = date.isoweekday() # Mon: 1 - Sun: 7
+  sleep['start_hour'] = start.time()
+  sleep['end_hour'] = end.time()
+  sleep['length'] = (end - start).total_seconds() / 3600
+  sleep['cycles'] = int(row[11])
+  sleep['deep'] = float(row[12])
+  return sleep
 
 def filter_events(keys, values):
   if len(keys) != len(values):
     raise Exception("Length of keys must match length of values")
-  events = []
+  filtered_columns = []
   for i in range(len(keys)):
     if keys[i] == 'Event':
-      events.append(values[i])
+      filtered_columns.append(values[i])
+  events = {}
+  for event in filtered_columns:
+    e = event.split('-')
+    if e[0] not in events:
+      events[e[0]] = []
+    events[e[0]].append(datetime.fromtimestamp(int(e[1])/1000))    
   return events
 
 if __name__ == "__main__":
